f867740c136c4ffb42256c2017b7069fa7639b5e
kunalArya1 <kunalkrraj220@gmail.com>
2025-01-15 20:20:22 +0530

    Prisma code added

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("Hello");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const client = new client_1.PrismaClient();
+function createuser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.user.create({
+            data: {
+                username: "kunal",
+                password: "12345",
+                age: 20,
+            },
+        });
+    });
+}
+createuser();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello");
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+import express from "express";
+
+async function createuser() {
+  await client.user.create({
+    data: {
+      username: "kunal",
+      password: "12345",
+      age: 20,
+    },
+  });
+}
+
+createuser();
